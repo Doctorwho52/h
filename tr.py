7f7fc587fc76e88d3396b7e8f4807d484994cdde
Doctorwho52 <kerimroyale31@gmail.com>
2025-08-27 21:32:29 +0300

    Update tr.py

--- tr.py
+++ tr.py
@@ -1,5 +1,3 @@
-
-
 from Kekik.cli import konsol
 from httpx     import Client
 from parsel    import Selector
@@ -47,10 +45,8 @@ class TRGoals:
         except Exception as e:
             raise ValueError(f"Redirect sırasında hata oluştu: {e}")
 
-        
         tum_url_listesi = [str(r.url) for r in response.history] + [str(response.url)]
 
-        
         for url in tum_url_listesi[::-1]:  
             if "trgoals" in url:
                 return url.strip("/")
@@ -64,21 +60,17 @@ class TRGoals:
             return domain
 
         try:
-            
             yeni_domain = check_domain(self.redirect_gec(eldeki_domain))
         except Exception:
             konsol.log("[red][!] `redirect_gec(eldeki_domain)` fonksiyonunda hata oluştu.")
             try:
-                
                 yeni_domain = check_domain(self.trgoals_domaini_al())
             except Exception:
                 konsol.log("[red][!] `trgoals_domaini_al` fonksiyonunda hata oluştu.")
                 try:
-                    
                     yeni_domain = check_domain(self.redirect_gec("https://t.co/MTLoNVkGQN"))
                 except Exception:
                     konsol.log("[red][!] `redirect_gec('https://t.co/MTLoNVkGQN')` fonksiyonunda hata oluştu.")
-                    
                     rakam = int(eldeki_domain.split("trgoals")[1].split(".")[0]) + 1
                     yeni_domain = f"https://trgoals{rakam}.xyz"
 
@@ -100,23 +92,28 @@ class TRGoals:
             raise ValueError("M3U dosyasında eski yayın URL'si bulunamadı!")
 
         eski_yayin_url = eski_yayin_url[0]
-        konsol.log(f"[yellow][~] Eski Yayın URL : {eski_yayin_url}")
 
         
-        response = self.httpx.get(kontrol_url, follow_redirects=True)
+        if eski_yayin_url.startswith("https://subarec.kerimmkirac.workers.dev"):
+            konsol.log("[blue][~] subarec.kerimmkirac.workers.dev bulundu, değiştirmeden devam ediliyor...")
+            yayin_url = eski_yayin_url
+        else:
+            konsol.log(f"[yellow][~] Eski Yayın URL : {eski_yayin_url}")
 
-        if not (yayin_ara := re.search(r'(?:var|let|const)\s+baseurl\s*=\s*"(https?://[^"]+)"', response.text)):
-            secici = Selector(response.text)
-            baslik = secici.xpath("//title/text()").get()
-            if baslik == "404 Not Found":
-                yeni_domain = eldeki_domain
-                yayin_ara   = [None, eski_yayin_url]
-            else:
-                konsol.print(response.text)
-                raise ValueError("Base URL bulunamadı!")
+            response = self.httpx.get(kontrol_url, follow_redirects=True)
 
-        yayin_url = yayin_ara[1]
-        konsol.log(f"[green][+] Yeni Yayın URL : {yayin_url}")
+            if not (yayin_ara := re.search(r'(?:var|let|const)\s+baseurl\s*=\s*"(https?://[^"]+)"', response.text)):
+                secici = Selector(response.text)
+                baslik = secici.xpath("//title/text()").get()
+                if baslik == "404 Not Found":
+                    yeni_domain = eldeki_domain
+                    yayin_ara   = [None, eski_yayin_url]
+                else:
+                    konsol.print(response.text)
+                    raise ValueError("Base URL bulunamadı!")
+
+            yayin_url = yayin_ara[1]
+            konsol.log(f"[green][+] Yeni Yayın URL : {yayin_url}")
 
         yeni_m3u_icerik = m3u_icerik.replace(eski_yayin_url, yayin_url)
         yeni_m3u_icerik = yeni_m3u_icerik.replace(eldeki_domain, yeni_domain)
